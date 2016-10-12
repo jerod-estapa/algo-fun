3757de0646a89f7e5d7ec388830f248690ec5eeb
jerodestapa <jerodestapa@gmail.com>
2016-10-11 23:20:48 -0500

    Added ipython directory

--- python/anagram.py
+++ python/anagram.py
@@ -2,3 +2,11 @@
 
 # Anagram Check Algorithm Solution
 
+# Given two strings, check to see if they are anagrams. An anagram is when the two strings can be written
+# using the exact same letters (so you can just rearrange the letters to get a different phrase or word).
+
+
+def anagram(s1, s2):
+    a1 = [s1].split
+    a2 = [s2].split
+
